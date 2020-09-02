8cf4c55d2b275e6cbdc8a84eb087aee92d6d4f03
fkubota <rkkubotay@gmail.com>
2020-09-02 22:21:06 +0900

    [Add]

--- pipeline.py
+++ pipeline.py
@@ -7,26 +7,10 @@ from src import utils
 from src import configuration as C
 from src import models
 from src.early_stopping import EarlyStopping
+from src.train import train
 # import src.early_stopping as es
 
 logger = logging.getLogger(__name__)
-
-def train(model, device, train_loader, optimizer, scheduler, loss_func):
-    model.train()
-    epoch_train_loss = 0
-    for batch_idx, (data, target) in enumerate(progress_bar(train_loader)):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_func(output, target)
-        loss.backward()
-        optimizer.step()
-        epoch_train_loss += loss.item()*data.size(0)
-    scheduler.step()
-    loss = epoch_train_loss / len(train_loader.dataset)
-    del data
-    return loss
-
 
 
 @hydra.main(config_path="./config/base_config.yaml")
@@ -64,23 +48,26 @@ def run(cfg: DictConfig) -> None:
         losses_valid = []
         epochs = []
         early_stopping = EarlyStopping(patience=12, verbose=True, path=save_path)
-        n_epoch = settings['globals']['num_epochs']
-# n_epoch = 50
-        # for epoch in progress_bar(range(1, n_epoch+1)):
-        #     print(f'\n epoch: {epoch} {time.ctime()}')
-        #     loss_train = train(model, device, train_loader, optimizer, scheduler, loss_func)
-        #     loss_valid, f_score_valid = get_epoch_loss_score(model, device, valid_loader, loss_func)
-        #     print(f'loss_train: {loss_train:.6f}, loss_valid: {loss_valid:.6f}, f1(macro): {f_score_valid:.6f}')
-        #     
-        #     epochs.append(epoch)
-        #     losses_train.append(loss_train)
-        #     losses_valid.append(loss_valid)
-        #     
-        #     early_stopping(loss_valid, model)
-        # 
-        #     if early_stopping.early_stop:
-        #         print("Early stopping")
-        #         break
+        n_epoch = cfg['globals']['num_epochs']
+
+        for epoch in progress_bar(range(1, n_epoch+1)):
+            print(f'\n epoch: {epoch} {time.ctime()}')
+            loss_train = train(
+                    model, device, train_loader, 
+                    optimizer, scheduler, loss_func)
+            loss_valid, f_score_valid = get_epoch_loss_score(
+                    model, device, valid_loader, loss_func)
+            print(f'loss_train: {loss_train:.6f}, loss_valid: {loss_valid:.6f}, f1(macro): {f_score_valid:.6f}')
+
+            epochs.append(epoch)
+            losses_train.append(loss_train)
+            losses_valid.append(loss_valid)
+
+            early_stopping(loss_valid, model)
+
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
 
 if __name__ == "__main__":
     run()
